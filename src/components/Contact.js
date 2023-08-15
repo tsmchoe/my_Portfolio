f3e744d2c709ffb2d039b085d4430032186c70b1
tsmchoe <39310629+tsmchoe@users.noreply.github.com>
2023-08-15 13:26:16 -0400

    Update Contact.js
    Added back div

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ import React from "react";
 export default function Contact() {
   return (
     <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form
           netlify
           name="contact"
