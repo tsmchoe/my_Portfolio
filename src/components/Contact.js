d5a6f74006e552853120d034ba2646b100fec23e
tsmchoe <39310629+tsmchoe@users.noreply.github.com>
2024-03-04 22:12:53 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,18 +5,45 @@ import React from "react";
 export default function Contact() {
   return (
     <section id="contact" className="relative">
-      <div className="relative mb-2">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/place/Baltimore,+MD/@39.2848102,-76.7028973,12z/data=!3m1!4b1!4m6!3m5!1s0x89c803aed6f483b7:0x44896a84223e758!8m2!3d39.2903848!4d-76.6121893!16zL20vMDk0anY?entry=ttu"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                tsmchoe@email.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">123-456-7890</p>
+            </div>
+          </div>
+        </div>
         <form
           netlify
           name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
-          <p className="leading-relaxed mb-2">
+          <p className="leading-relaxed mb-5">
             Looking for a skilled and motivated Software Developer to enhance your team? Look no further. With a strong background in Java, Python, and JavaScript, I am ready to bring innovation and efficiency to your projects. Let's collaborate and achieve greatness together. Hire me today for a brighter tomorrow.
           </p>
-          <div className="relative mb-2">
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                        <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
             </label>
@@ -27,7 +54,7 @@ export default function Contact() {
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
-          <div className="relative mb-2">
+          <div className="relative mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-gray-400">
               Email
             </label>
@@ -38,7 +65,7 @@ export default function Contact() {
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
-          <div className="relative mb-2">
+          <div className="relative mb-4">
             <label
               htmlFor="message"
               className="leading-7 text-sm text-gray-400">
